35a5c84fd333385788e9ed919ae2737a173734fe
brianmina <brian.mina17@gmail.com>
2024-01-08 15:46:10 -0500

    template for edit an entry was created

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 
-from .models import Topic
+from .models import Topic, Entry
 from .forms import TopicForm, EntryForm
 
 
@@ -55,7 +55,7 @@ def new_entry(request, topic_id):
         # POST data submitted: process data.
         form = EntryForm(data=request.POST)
         if form.is_valid():
-            new_entry = form.save()
+            new_entry = form.save(commit=False)
             new_entry.topic = topic
             new_entry.save()
             return redirect('learning_logs:topic', topic_id=topic_id)
@@ -63,5 +63,24 @@ def new_entry(request, topic_id):
     # Display a blank or invalid form.
     context = {'topic': topic, 'form': form}
     return render(request, 'learning_logs/new_entry.html', context)
+
+
+def edit_entry(request, entry_id):
+    """Edit an existing entry."""
+    entry = Entry.objects.get(id=entry_id)
+    topic = entry.topic
+
+    if request.method != 'POST':
+        # Initial request: ore-fill form with the current entry.
+        form = EntryForm(instance=entry)
+    else:
+        # POST data submitted: process data.
+        form = EntryForm(instance=entry, data=request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('learning_logs:topic', topic_id=topic.id)
+
+    context = {'entry': entry, 'topic': topic, 'form': form}
+    return render(request, 'learning_logs/edit_entry.html', context)
 
 
